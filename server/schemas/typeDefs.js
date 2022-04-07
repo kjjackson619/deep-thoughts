19074c56403aa60377bd27231e3f9c404c8757e6
KJ Jackson <kjjackson619@yahoo.com>
2022-04-07 11:56:59 -0400

    mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,15 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+}
+
 type Thought {
     _id: ID
     thoughtText: String
@@ -28,10 +37,26 @@ type User {
 }
 
 type Query {
+    me: User
     users: [User]
     user(username: String!) : User
   thoughts(username: String): [Thought]
   thought(_id: ID!): Thought
-}`;
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+`;
 
 module.exports = typeDefs;
