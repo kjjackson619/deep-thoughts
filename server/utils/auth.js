74e9597046578b228d78340b5d82fa1c45ea9e5a
KJ Jackson <kjjackson619@yahoo.com>
2022-04-08 11:18:29 -0400

    forms

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,26 +1,23 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretsshhhhh'
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    signToken: function ({ username, email, _id }) {
-        const payload = { username, email, _id };
-
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
     authMiddleware: function ({ req }) {
+        // allows token to be sent via req.body, req.query, or headers
         let token = req.body.token || req.query.token || req.headers.authorization;
 
-        if (req.headers.authorizaton) {
+        // ["Bearer", "<tokenvalue>"]
+        if (req.headers.authorization) {
             token = token
-                .split('')
+                .split(' ')
                 .pop()
                 .trim();
         }
 
         if (!token) {
-            return req
+            return req;
         }
 
         try {
@@ -31,5 +28,10 @@ module.exports = {
         }
 
         return req;
+    },
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     }
 };
